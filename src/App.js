b715e92a82ecb6f15083140c0560ef528d588a50
ronny christensen <6382104@rts-365.dk>
2021-08-16 09:40:47 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // useEffect(function () {
+  //   Notification.requestPermission(function(status) {
+  //     console.log('Notification permission status:', status);
+  // });
+  // },[])
+
+
+
+
+  function displayNotification() {
+    if(Notification.permission === "granted") {
+      navigator.serviceWorker.getRegistration()
+      .then(function(reg) {
+        reg.showNotification("yo man");
+      });
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => displayNotification()}>Notify me</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,4 +43,8 @@ function App() {
   );
 }
 
+
+Notification.requestPermission(function(status) {
+  console.log('Notification permission status:', status);
+});
 export default App;
